f47a079975aa963be4ce809f2df8713ceb9affcc
RafaIbarra <blasrafael1986@gmail.com>
2026-09-14 08:48:45 -0300

    CORS app 3

--- main.py
+++ main.py
@@ -135,7 +135,7 @@ app.add_middleware(
     allow_origins=origins,
     allow_credentials=True,
     allow_methods=["GET", "POST", "PUT", "DELETE","OPTIONS"],
-    allow_headers=["Authorization", "Content-Type", "X-CSRF-Token"],
+    allow_headers=["Authorization", "Content-Type", "X-CSRF-Token","X-Session-User"],
 )
 
 
